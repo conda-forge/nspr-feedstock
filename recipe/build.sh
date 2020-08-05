bc04a66ea3e132f3aa6221061a0392537cd78260
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2020-08-05 21:32:27 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd nspr
-sed -ri 's#^(RELEASE_BINS =).*#\1#' pr/src/misc/Makefile.i
+sed -ri 's#^(RELEASE_BINS =).*#\1#' pr/src/misc/Makefile.in
 sed -i 's#$(LIBRARY) ##'            config/rules.mk
 
 ./configure --prefix="${PREFIX}" --enable-64bit --disable-debug --enable-optimize --with-pthreads --with-mozilla
