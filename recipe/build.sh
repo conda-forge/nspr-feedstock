b2d5c2a210e1848a27dc43f09017ff8398c6a2ef
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2020-08-05 18:57:31 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 cd nspr
+sed -ri 's#^(RELEASE_BINS =).*#\1#' pr/src/misc/Makefile.i
+sed -i 's#$(LIBRARY) ##'            config/rules.mk
 
-./configure --prefix="${PREFIX}" --enable-64bit --disable-debug --enable-optimize
+./configure --prefix="${PREFIX}" --enable-64bit --disable-debug --enable-optimize --with-pthreads --with-mozilla
 
 make -j $CPU_COUNT
 make install
