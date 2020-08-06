43c0fa4cb6be5b4a484c9bdeb61d9782057829ba
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2020-08-06 06:49:35 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
-OSNAME=$(uname)
 
 cd nspr
-PTHREADS_OPTIONS=""
-if [ "${OSNAME}" == Linux ]; then
-    sed -ri 's#^(RELEASE_BINS =).*#\1#' pr/src/misc/Makefile.in
-    sed -i 's#$(LIBRARY) ##'            config/rules.mk
-    PTHREADS_OPTIONS="--with-pthreads"
-fi
 
+sed -ri 's#^(RELEASE_BINS =).*#\1#' pr/src/misc/Makefile.in
+sed -i 's#$(LIBRARY) ##'            config/rules.mk
 
-./configure --prefix="${PREFIX}" --enable-64bit --disable-debug --enable-optimize ${PTHREADS_OPTIONS} --with-mozilla
+./configure --prefix="${PREFIX}" --enable-64bit --disable-debug --enable-optimize --with-pthreads --with-mozilla
 
 make -j $CPU_COUNT
 make install
