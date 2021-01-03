68f6e19cff4542e2fce65431bfd2a4a33355e34c
Isuru Fernando <isuruf@gmail.com>
2021-01-02 20:44:35 -0600

    Print config.log

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ cd nspr
 sed -ri 's#^(RELEASE_BINS =).*#\1#' pr/src/misc/Makefile.in
 sed -i 's#$(LIBRARY) ##'            config/rules.mk
 
-./configure --prefix="${PREFIX}" --enable-64bit --disable-debug --enable-optimize --with-pthreads --with-mozilla
+./configure --prefix="${PREFIX}" --enable-64bit --disable-debug --enable-optimize --with-pthreads --with-mozilla || (cat config.log; false)
 
 make -j $CPU_COUNT
 make install
